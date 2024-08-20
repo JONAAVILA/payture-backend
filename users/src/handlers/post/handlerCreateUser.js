bb87daa9b93f4d509658a40d031091026316e9e1
JONAAVILA <jona_1969@hotmail.com>
2024-08-20 17:24:27 -0300

    handler create user fixed and encrypted

--- users/src/handlers/post/handlerCreateUser.js
+++ users/src/handlers/post/handlerCreateUser.js
@@ -1,13 +1,28 @@
 import { models } from '../../db.js';
+import { schema } from '../schema.js';
+import bcrypt from 'bcrypt';
 const { User } = models;
 
+const SALT_ROUNDS = Number(process.env)
+
 const handleCreateUser = async (id,name,userName,email,password )=>{
+    const { error } = schema.validate({
+        uuid:id,
+        name:name,
+        userName:userName,
+        email:email,
+        password:password
+    })
+    if(error) throw new Error('Invalid parameters')
+
+    const passwordHashed = await bcrypt.hashSync(password,SALT_ROUNDS)
+
     const response = await User.create({
         id:id,
         name:name,
         userName:userName,
         email:email,
-        password:password
+        password:passwordHashed
     })
     if(!response) throw new Error('Error to create')
     return 'User created'
