fed2568d45ffd9aa4233fe175abe8cd0c631b848
JONAAVILA <jona_1969@hotmail.com>
2024-08-23 10:31:25 -0300

    handlerSendToken fixed message

--- users/src/handlers/post/validations/tokens/handlerSendToken.js
+++ users/src/handlers/post/validations/tokens/handlerSendToken.js
@@ -3,16 +3,18 @@ import createCode from './createCode.js';
 
 const { SECRET_KEY } = process.env
 
-const handlerCheckToken = async (token)=>{
+const handlerSendToken = async (token)=>{
     try {
-        jwt.verify(token,SECRET_KEY, async (err,encoded)=>{
+        const codes = jwt.verify(token,SECRET_KEY, async (err,encoded)=>{
             if(err) throw new Error('Invalid access');  
             const codes = await createCode(encoded.email)
-            if(codes) return 'Token was send'
+            return codes
         })
+        if(!codes) throw new Error('Something went wrong');
+        return 'Token was send'
     } catch (error) {
         return {error:error.message}
     }
 }
 
-export default handlerCheckToken
+export default handlerSendToken
